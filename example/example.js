7f51e881069506feba2151f73a2a50549d92b4ac
ishan-marikar <ishan@imarikar.com>
2018-09-29 13:28:32 +0530

    Add feature to retry on fail

--- example/example.js
+++ example/example.js
@@ -2,8 +2,23 @@ const examResults = require("../");
 
 // DISCLAIMER: Got these index numbers randomly on facebook.
 // PR or pull up an issue if you want it taken down.
-examResults.getCurrentExams().then(console.log);
-examResults.getALResults("5026024").then(console.log);
-examResults.getOLResults("60207698").then(console.log);
-examResults.getOLResults("60043156").then(console.log);
-examResults.getGrade5Results("2905809").then(console.log);
+examResults
+  .getCurrentExams()
+  .then(console.log)
+  .catch(console.error);
+examResults
+  .getALResults("5026024")
+  .then(console.log)
+  .catch(console.error);
+examResults
+  .getOLResults("60207698")
+  .then(console.log)
+  .catch(console.error);
+examResults
+  .getOLResults("60043156")
+  .then(console.log)
+  .catch(console.error);
+examResults
+  .getGrade5Results("2905809")
+  .then(console.log)
+  .catch(console.error);
