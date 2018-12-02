dea225ca4a404200713ce060f0dc760b056f1128
ishan-marikar <ishan@imarikar.com>
2018-12-02 20:49:35 +0530

    Update methods to use async/await

--- example/example.js
+++ example/example.js
@@ -2,22 +2,31 @@ const examResults = require("../");
 
 // DISCLAIMER: Got these index numbers randomly on facebook.
 // PR or pull up an issue if you want it taken down.
+console.log("Get current exams..");
 examResults
   .getCurrentExams()
   .then(console.log)
   .catch(console.error);
+
+console.log("Get A/L results..");
 examResults
   .getALResults("5026024")
   .then(console.log)
   .catch(console.error);
+
+console.log("Get O/L results..");
 examResults
   .getOLResults("60207698")
   .then(console.log)
   .catch(console.error);
+
+console.log("Get O/L results..");
 examResults
   .getOLResults("60043156")
   .then(console.log)
   .catch(console.error);
+
+console.log("Get Grade 5 results..");
 examResults
   .getGrade5Results("2905809")
   .then(console.log)
